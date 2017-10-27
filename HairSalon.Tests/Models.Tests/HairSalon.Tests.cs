cae49c3afe3b73e836701fa2490eccd206827c2a
Kimberly Bordon <kbordon@gmail.com>
2017-10-27 08:17:05 -0700

    Create project solution folder structure.

---
+++ HairSalon.Tests/Models.Tests/HairSalon.Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+  [TestClass]
+  public class HairSalonTest
+  {
+    [TestMethod]
+    public void Method_Description_ExpectedValue()
+    {
+      Assert.AreEqual(var1, method(input));
+    }
+  }
+}
