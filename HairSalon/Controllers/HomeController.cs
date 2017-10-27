0ecfae16374afc731ab91b8e223028a0c7e327d2
Kimberly Bordon <kbordon@gmail.com>
2017-10-27 14:22:33 -0700

    Add form to enter stylist, and client.

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -5,46 +5,91 @@ using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
     {
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+      return View();
+    }
 
-      [HttpGet("/stylists")]
-      public ActionResult StylistsView()
+    [HttpGet("/stylists")]
+    public ActionResult StylistsView()
+    {
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View("Stylists", allStylists);
+    }
+
+    [HttpGet("/stylists/new")]
+    public ActionResult StylistForm()
+    {
+      // string error = "";
+      return View();
+    }
+
+    [HttpPost("/stylists/new")]
+    public ActionResult StylistAdd()
+    {
+      double validNumber;
+      bool correctPhone = double.TryParse(Request.Form["stylist-phone"], out validNumber);
+      if (correctPhone)
       {
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-phone"]);
+        newStylist.Save();
         List<Stylist> allStylists = Stylist.GetAll();
         return View("Stylists", allStylists);
       }
-
-      [HttpGet("/stylists/new")]
-      public ActionResult StylistForm()
+      else
       {
-        // string error = "";
-        return View();
+        string error = "error";
+        return View("StylistForm", error);
       }
-
-      [HttpPost("/stylists/new")]
-      public ActionResult StylistsAdd()
-      {
-        double validNumber;
-        bool correctPhone = double.TryParse(Request.Form["stylist-phone"], out validNumber);
-        if (correctPhone)
-        {
-          Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-phone"]);
-          newStylist.Save();
-          List<Stylist> allStylists = Stylist.GetAll();
-          return View("Stylists", allStylists);
-        }
-        else
-        {
-          string error = "error";
-          return View("StylistForm", error);
-        }
-      }
-
     }
+
+    [HttpGet("/stylists/{id}")]
+    public ActionResult StylistDetail(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>{};
+      model.Add("selected-client", null);
+      Stylist selectedStylist = Stylist.Find(id);
+      model.Add("this-stylist", selectedStylist);
+      List<Client> stylistClients = Client.GetAllClientsByStylist(selectedStylist.Id);
+      model.Add("stylist-clients", stylistClients);
+      return View(model);
+    }
+
+    [HttpGet("/stylists/{id}/clients/new")]
+    public ActionResult ClientForm(int id)
+    {
+      Stylist selectedStylist = Stylist.Find(id);
+      return View(selectedStylist);
+    }
+
+
+    [HttpPost("/stylists/{id}/clients/new")]
+    public ActionResult ClientAdd(int id)
+    {
+      double validNumber;
+      bool correctPhone = double.TryParse(Request.Form["client-phone"], out validNumber);
+      if (correctPhone)
+      {
+        Client newClient = new Client(Request.Form["client-name"], Request.Form["client-phone"], id);
+        newClient.Save();
+
+        Dictionary<string, object> model = new Dictionary<string, object> {};
+        model.Add("selected-client", newClient);
+        Cuisine selectedCuisine = Cuisine.Find(id);
+        model.Add("this-stylist", selectedCuisine);
+        List<Client> allClients = Client.GetAllClientsByCuisine(id);
+        model.Add("stylist-clients", allClients);
+        return View("StylistDetail", model);
+      }
+      else
+      {
+        string error = "error";
+        return View("ClientForm", error);
+      }
+    }
+
+  }
 }
